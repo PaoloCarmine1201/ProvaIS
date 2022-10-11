83e9d5cc3d944361beb647779cf9cff2e56c4006
PaoloCarmine1201 <paolocarmine1201@gmail.com>
2022-10-11 10:09:49 +0200

    aggiunta calcolatrice

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello" );
+        double operando1 = 20;
+        double operando2 = 5;
+
+        System.out.println(Calcolatrice.somma(operando1, operando2));
+        System.out.println(Calcolatrice.sottrazione(operando1, operando2));
+        System.out.println(Calcolatrice.moltiplicazione(operando1, operando2));
+        System.out.println(Calcolatrice.divisione(operando1, operando2));
     }
 }
